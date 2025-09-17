3101ff5f4300d7c0a9800d3a3589c27c0d509104
Saksham Jaiswal <sakshamjaiswalofficial@gmail.com>
2025-09-17 08:03:30 +0530

    Reading and Writing Solidity

--- contracts/MyContract.sol
+++ contracts/MyContract.sol
@@ -4,4 +4,8 @@ pragma solidity 0.8.30;
 
 contract MyContract {
     string public ourString = "Hello World";
+    
+    function updateOurString(string memory _updatedString) public {
+        ourString = _updatedString;
+    }
 }
